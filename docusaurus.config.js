fe146f42c6c67b89c8191606beb26bba9111d854
Matthew <real_mf366@yahoo.com>
2026-07-23 17:06:15 +0100

    chore: Setup docs configs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
   organizationName: 'OceanApocalypseStudios', // Usually your GitHub org/user name.
   projectName: 'RedSeaMarkupLanguage', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn', // TODO: set to throw in prod
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
